8956c03b6c5a5613890bbf3fd9d31870f821d2bc
rioaldie <rioaldierwanto@gmail.com>
2025-01-24 17:51:12 +0700

    implement qstash user onboarding

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
-    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
@@ -9,11 +9,12 @@ const config = {
     },
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
-      redisUrl: process.env.UPSTAH_REDIS_URL,
-      redisToken: process.env.UPSTASH_REDIS_TOKEN,
-      qstashUrl: process.env.QSTASH_URL,
-      qstashToken: process.env.QSTASH_TOKEN,
+      redisUrl: process.env.UPSTAH_REDIS_URL!,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
     },
+    resendToken: process.env.RESEND_TOKEN!,
   },
 };
 
